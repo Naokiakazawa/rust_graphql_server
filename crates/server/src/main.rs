8ee6814351536e3dc27165ce71f417e20a97afe6
Naokiakazawa <naoki9akazawa@gmail.com>
2024-07-08 10:07:22 +0900

    add essential libraries

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,17 @@
+use dotenvy::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_test_writer()
+        .init();
+    match dotenv() {
+        Ok(_) => tracing::info!("Loaded .env file"),
+        Err(e) => tracing::error!("Failed to load .env file: {}", e),
+    }
+    let server_url: String = env::var("SERVER_URL").unwrap();
+    let server_port: String = env::var("SERVER_PORT").unwrap();
+    println!("Server URL: {}", server_url);
+    println!("Server Port: {}", server_port);
 }
