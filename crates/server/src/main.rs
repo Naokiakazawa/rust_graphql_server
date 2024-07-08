78b9f8dff69293ad56be97838a946768d04f288c
Naokiakazawa <naoki9akazawa@gmail.com>
2024-07-08 11:12:33 +0900

    WIP: add async-graphql

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,7 +1,39 @@
+use actix_web::{guard, web, App, HttpResponse, HttpServer, Result};
+use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, Schema};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
 use dotenvy::dotenv;
+use sea_orm::{Database, DatabaseConnection};
 use std::env;
 
-fn main() {
+struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    async fn hello(&self) -> String {
+        "Hello, world!".to_string()
+    }
+}
+
+type AppSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+
+async fn index(schema: web::Data<AppSchema>, req: GraphQLRequest) {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn index_graphiql() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(GraphiQLSource::build().endpoint("/").finish()))
+}
+
+async fn establish_db_connection() -> Result<DatabaseConnection, Error> {
+    let db_url: String = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db: DatabaseConnection = Database::connect(&db_url).await?;
+    Ok(db)
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .with_test_writer()
@@ -12,6 +44,18 @@ fn main() {
     }
     let server_url: String = env::var("SERVER_URL").unwrap();
     let server_port: String = env::var("SERVER_PORT").unwrap();
-    println!("Server URL: {}", server_url);
-    println!("Server Port: {}", server_port);
+
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription);
+
+    tracing::info!("GraphiQL IDE: http://localhost:8000");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(schema.clone()))
+            .service(web::resource("/graphql").guard(guard::Post()).to(index))
+            .service(web::resource("/").guard(guard::Get()).to(index_graphiql))
+    })
+    .bind(format!("{}:{}", server_url, server_port))?
+    .run()
+    .await
 }
